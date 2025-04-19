74bec5b055be351c5f5ab09b9e55de9af95302f9
lochan paudel <lochanpaudel10@gmail.com>
2025-04-19 23:01:37 +0530

    exceptions/DuplicateStudentException.java created

---
+++ exceptions/DuplicateStudentException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+// DuplicateStudentException.java
+public class DuplicateStudentException extends CustomException {
+  public DuplicateStudentException(String msg) {
+    super(msg);
+  }
+}
